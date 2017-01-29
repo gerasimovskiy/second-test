4dcf8fdc05feea6531b10e3c1145419527315321
Deni <dennni81@gmail.com>
2017-01-30 00:05:05 +0200

    added forms js

--- js/main.js
+++ js/main.js
@@ -31,4 +31,58 @@
 		});
 	});
 	});
+
+
+
+
+	function valid(el, pattern, msgEl) {
+		var res = el.val().search(pattern);
+		if (res == -1) {
+			return msgEl;
+		} else {
+			return '';
+		}
+	};
+
+	function validPassword(el) {
+		var msgEl = '',
+			res1 = el.val().search(/[A-Z]/),
+			res2 = el.val().search(/[a-z]/),
+			res3 = el.val().search(/[0-9]/);
+		if (res1 == -1 || res2 == -1 || res3 == -1) {
+			msgEl += 'Use also ';
+			if (res1 == -1) msgEl += 'uppercase letters';
+			if (res1 == -1 && res2 == -1) msgEl += ', ';
+			if (res2 == -1) msgEl += 'lowercase letters';
+			if (res1 == -1 && res3 == -1 || res2 == -1 && res3 == -1) msgEl += ', ';
+			if (res3 == -1) msgEl += 'number';
+			msgEl += ' in the password.';
+		};
+		return msgEl;
+	}
+
+	$('.trynow-form__btn').on('click', function(e) {
+		var msg = '';
+		msg += valid($('[name="name"]'),/^[a-zA-Z0-9]{1,}$/,'Use Latin letters and numbers in the name. ');
+		msg += valid($('[name="email"]'),/^[a-z0-9._]+@[a-z0-9.-]+\.[a-z]{2,4}$/,'E-mail isn\'t correct. ');
+		msg += validPassword($('[name="pass"]'));
+		
+		if (msg != '') {
+			alert(msg);
+			return false;
+		}
+	});
+
+	// $('.footer__button').on('click', function(e) {
+	// 	var msg = '';
+	// 	msg += valid($('[name="emailnews"]'),/^[a-z0-9._]+@[a-z0-9.-]+\.[a-z]{2,4}$/,'E-mail isn\'t correct.');
+
+	// 	if (msg != '') {
+	// 		alert(msg);
+	// 		return false;
+	// 	}
+	// });
+
+
+
 })(jQuery);
